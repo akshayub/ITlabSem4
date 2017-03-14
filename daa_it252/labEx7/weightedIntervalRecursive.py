c7fba296a0c85cdde4e623c8881d9cfb1c950dc9
Akshay Upadhya B <akshayupadhya+git@gmail.com>
2017-03-14 14:57:49 +0530

    Incomplete

---
+++ daa_it252/labEx7/weightedIntervalRecursive.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+# Top down approach for getting max weight of all non-overlapping intervals.
+# By the method of Dynamic programming.
+
+class Interval(object):
+    start = 0
+    end = 0
+    weight = 0
+    def __init__(self, vals):
+        self.start = vals[0]
+        self.end = vals[1]
+        self.weight = vals[2]
+
+def getPrev(intervals):
+    ans = [None] * len(intervals)
+    for x in xrange(len(intervals)):
+        try:
+            map(lambda x:x.end, intervals)
+        except Exception:
+            ans[x] = None
+    return ans
+
+def optimalSol(intervals, prevs):
+    ans = 0
+    pass
+    print prevs
+    return ans
+
+
+def main():
+    # intervals = input("Number of intervals? ")
+    intervals = input()
+    allIntervals = []
+    # print 'Enter start, end, weight of intervals'
+    for interval in xrange(intervals):
+        # start, end, weight = [int(x) for x in raw_input().split()]
+        allIntervals.append(
+            Interval(
+                [int(x) for x in raw_input().split()]
+            )
+        )
+    allIntervals.sort(key = lambda x:x.end)
+
+    print optimalSol(allIntervals, getPrev(allIntervals))
+
+if __name__ == '__main__':
+    main()
