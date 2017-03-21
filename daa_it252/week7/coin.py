55a40b938db9aa62cf22003cd58c74453004c622
Akshay Upadhya B <akshayupadhya+git@gmail.com>
2017-03-21 13:58:06 +0530

    added extra feature

--- daa_it252/week7/coin.py
+++ daa_it252/week7/coin.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
-
+coinsCount = {}
 def sol(n, coins):
+    global coinsCount
     finalNumber = float('inf')
     coins.sort(reverse=True)
     val = n
     while coins != []:
         number = 0
         n = val
+        temp = {}
         for coin in coins:
             if n >= coin:
                 number += n/coin
+                temp[coin] = n/coin
                 n %= coin
+        if min(finalNumber, number) == number:
+            coinsCount = dict(temp)
         finalNumber = min(finalNumber, number)
         coins.remove(coins[0])
     return finalNumber
@@ -19,6 +24,9 @@ def main():
     coins = [int(x) for x in raw_input("Enter coins: ").split()]
     n = input("Enter value: ")
     print "Min coins",sol(n, coins)
+    # print coinsCount
+    for coin in coinsCount:
+        print 'Coin of value', coin, 'used ', coinsCount[coin], 'times'
 
 if __name__ == '__main__':
     main()
