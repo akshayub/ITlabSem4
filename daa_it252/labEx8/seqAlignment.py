821015c59c56d1763f8d161b8f24b376baf981e2
Akshay Upadhya B <akshayupadhya+git@gmail.com>
2017-03-27 16:52:48 +0530

    Worked on edit distance of string and updated readme of assignment 9

--- daa_it252/labEx8/seqAlignment.py
+++ daa_it252/labEx8/seqAlignment.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
+from sys import argv
 
-def findEdit(x,y):
-    
+def diff(x,y):
+    if x == y:
+        return 0
+    else:
+        return 1
 
-str1 = raw_input()
-str2 = raw_input()
-findEdit(str1, str2)
+def findEdit(x,i,y,j):
+    if i < len(x) and j < len(y):
+        ans1 = diff(x[i], y[j]) + findEdit(x,i+1,y,j+1)
+        ans2 = diff(x[i], None) + findEdit(x,i+1,y,j)
+        ans3 = diff(None, y[j]) + findEdit(x,i,y,j+1)
+        ans = min(ans1, ans2, ans3)
+        return ans
+    else:
+        return 0
+
+print "Cost is ", findEdit(argv[1], 0, argv[2], 0)
